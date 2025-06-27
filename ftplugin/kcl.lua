ee7c3ebc38664ce02682ae7586449615202751cc
Vladimír Dudr <dudr@metrans.cz>
2025-06-27 17:11:01 +0200

    remove fold options
    those are user specific

--- ftplugin/kcl.lua
+++ ftplugin/kcl.lua
@@ -1,10 +1,7 @@
-vim.api.nvim_command([[autocmd FileType kcl set nofoldenable]])
-vim.api.nvim_command([[autocmd FileType kcl set foldmethod=syntax]])
-
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
 vim.bo.tabstop = 4
 vim.bo.softtabstop = 4
-vim.bo.commentstring = "#%s"
+vim.bo.commentstring = "# %s"
 
 vim.b.undo_ftplugin = "setl commentstring< sw< ts< sts< et<"
