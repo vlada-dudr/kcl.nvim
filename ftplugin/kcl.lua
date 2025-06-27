17ce7c81525db65934b06e79ed1a660c7d75952f
Vladimír Dudr <dudr@metrans.cz>
2025-06-27 17:10:06 +0200

    add undo_ftplugin buffer var
    to follow conventions

--- ftplugin/kcl.lua
+++ ftplugin/kcl.lua
@@ -5,5 +5,6 @@ vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
 vim.bo.tabstop = 4
 vim.bo.softtabstop = 4
-
 vim.bo.commentstring = "#%s"
+
+vim.b.undo_ftplugin = "setl commentstring< sw< ts< sts< et<"
